e5776c1d151345dcc256052f67224eacab4ba400
John Mettraux <jmettraux@gmail.com>
2019-06-14 14:15:57 +0900

    Prepare 0.1.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,9 @@
 # CHANGELOG.md
 
 
+## avantage 0.1.0  not yet released
+
+
 ## avantage 0.0.1  released 2019-06-14
 
 * Initial release
--- lib/avantage.rb
+++ lib/avantage.rb
@@ -1,7 +1,7 @@
 
 module Avantage
 
-  VERSION = '0.0.1'
+  VERSION = '0.1.0'
 
   USER_AGENT = "avantage - #{VERSION} - https://github.com/jmettraux/avantage"
 end
