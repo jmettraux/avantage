1f44bf42518436cd537ec53b0d484c192b9b49c6
John Mettraux <jmettraux@gmail.com>
2019-06-14 18:52:39 +0900

    Implement backbone

--- lib/avantage.rb
+++ lib/avantage.rb
@@ -1,8 +1,114 @@
 
+require 'json'
+require 'net/http'
+
+
 module Avantage
 
   VERSION = '0.1.0'
 
-  USER_AGENT = "avantage - #{VERSION} - https://github.com/jmettraux/avantage"
+  USER_AGENT = "avantage #{VERSION} - https://github.com/jmettraux/avantage"
+  API_ROOT_URI = 'https://www.alphavantage.co/query?'
+
+  class Client
+
+    def initialize(api_key)
+
+      @api_key = api_key
+    end
+
+    def self.add_endpoint(key, aliaz, required_parameters, csv=true)
+
+      k = key.downcase
+
+      define_method(k) { |opts={}|
+        get(key, required_parameters, opts) }
+      define_method("#{k}_csv") { |opts={}|
+        get(key, required_parameters, opts.merge!(dataType: 'csv')) } if csv
+
+      if aliaz
+        alias_method(aliaz, k)
+        alias_method("#{aliaz}_csv", "#{k}_csv") if csv
+      end
+    end
+
+    {
+      TIME_SERIES_INTRADAY: [ nil, %w[ symbol interval ] ],
+      TIME_SERIES_DAILY: [ nil, %w[ symbol ] ],
+      TIME_SERIES_DAILY_ADJUSTED: [ nil, %w[ symbol ] ],
+      TIME_SERIES_WEEKLY: [ nil, %w[ symbol ] ],
+      TIME_SERIES_WEEKLY_ADJUSTED: [ nil, %w[ symbol ] ],
+      TIME_SERIES_MONTHLY: [ nil, %w[ symbol ] ],
+      TIME_SERIES_MONTHLY_ADJUSTED: [ nil, %w[ symbol ] ],
+      GLOBAL_QUOTE: [ :quote, %w[ symbol ] ],
+      SYMBOL_SEARCH: [ :search, %w[ keywords ] ],
+    }.each { |k, v| add_endpoint(k, *v) }
+
+    {
+      CURRENCY_EXCHANGE_RATE: [ :fx, %w[ from_currency to_currency ] ],
+      FX_INTRADAY: [ nil, %w[ from_symbol to_symbol interval ] ],
+      FX_DAILY: [ nil, %w[ from_symbol to_symbol ] ],
+      FX_WEEKLY: [ nil, %w[ from_symbol to_symbol ] ],
+      FX_MONTHLY: [ nil, %w[ from_symbol to_symbol ] ],
+    }.each { |k, v| add_endpoint(k, *v) }
+
+    {
+      #CURRENCY_EXCHANGE_RATE: [ nil, %w[ from_currency to_currency ] ],
+      DIGITAL_CURRENCY_DAILY: [ nil, %w[ symbol market ] ],
+      DIGITAL_CURRENCY_WEEKLY: [ nil, %w[ symbol market ] ],
+      DIGITAL_CURRENCY_MONTHLY: [ nil, %w[ symbol market ] ],
+    }.each { |k, v| add_endpoint(k, *v) }
+
+    {
+      # TODO Technical Indicators
+    }.each { |k, v| add_endpoint(k, *v) }
+
+    {
+      SECTOR: [ :sectors, %w[] ],
+    }.each { |k, v| add_endpoint(k, *v) }
+
+    protected
+
+    def get(key, required_parameters, opts)
+
+#p [ key, required_parameters, opts ]
+#TODO check required parameters
+
+      os = opts.merge(function: key, apikey: @api_key)
+
+      uri = API_ROOT_URI + URI.encode_www_form(os)
+
+      req = Net::HTTP::Get.new(uri)
+      req.instance_eval { @header.clear }
+      def req.set_header(k, v); @header[k] = [ v ]; end
+
+      req.set_header('User-Agent', USER_AGENT)
+
+      u = URI(uri)
+
+      t0 = monow
+
+      t = Net::HTTP.new(u.host, u.port)
+      t.use_ssl = (u.scheme == 'https')
+#t.set_debug_output($stdout)
+
+      res = t.request(req)
+
+      r = res.body
+      if res.content_type.match(/\Aapplication\/json\b/)
+        r = JSON.parse(r)
+      end
+
+      class << r; attr_accessor :_response, :_client, :_elapsed; end
+        #
+      r._response = res
+      r._client = self
+      r._elapsed = monow - t0
+
+      r
+    end
+
+    def monow; Process.clock_gettime(Process::CLOCK_MONOTONIC); end
+  end
 end
 
---
+++ spec/avantage_client_spec.rb
@@ -0,0 +1,50 @@
+
+#
+# Specifying avantage
+#
+# Fri Jun 14 17:56:24 JST 2019
+#
+
+require 'spec_helper'
+
+
+describe Avantage::Client do
+
+  before :each do
+
+    @client = Avantage::Client.new(File.read('.api.key').strip)
+  end
+
+  describe '#quote' do
+  end
+
+  describe '#search' do
+  end
+
+  describe '#sectors' do
+
+    it 'returns sector performances' do
+
+      r = @client.sectors
+
+      expect(r.class).to eq(Hash)
+
+      expect(
+        r.keys
+      ).to eq([
+        'Meta Data',
+        'Rank A: Real-Time Performance',
+        'Rank B: 1 Day Performance',
+        'Rank C: 5 Day Performance',
+        'Rank D: 1 Month Performance',
+        'Rank E: 3 Month Performance',
+        'Rank F: Year-to-Date (YTD) Performance',
+        'Rank G: 1 Year Performance',
+        'Rank H: 3 Year Performance',
+        'Rank I: 5 Year Performance',
+        'Rank J: 10 Year Performance'
+      ])
+    end
+  end
+end
+
